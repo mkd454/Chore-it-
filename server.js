d6f5f014420d7ed1a5ffb2920e0535ab95e40b73
Mary Dang <dangmaryk@gmail.com>
2019-05-08 17:34:56 -0500

    missing index.js in models and changing db name

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
+// Require all models
+const db = require("./models");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,16 +15,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3");
+
 // Define API routes here
+db.Group.create({ name: "Group1Test" })
+  .then(function(dbGroup) {
+    console.log(dbGroup);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);

1935d35780a6ba41ff31c8261c85a41422934a0c
Matthew Trabold <mct4240@gmail.com>
2019-05-09 20:23:55 -0500

    Updated the Animation
    Animation for the login button is now even more awesome!

--- client/src/components/authButtons/LoginButton.js
+++ client/src/components/authButtons/LoginButton.js
@@ -4,9 +4,30 @@ import posed from 'react-pose';
 import { AuthConsumer } from "../../utils/Auth/authContext";
 
 const Button = posed.div({
+  hoverable: true,
   pressable: true,
-  init: { scale: 1 },
-  press: { scale: 0.8 }
+  init: {
+    scale: 1,
+    boxShadow: '0px 0px 0px rgba(0,0,0,0)',
+    backgroundColor: '#1A3766'
+  },
+  hover: {
+    scale: 1.2,
+    boxShadow: '0px 0px 5px rgba(0,0,0,.5)',
+    backgroundColor: '#1a378e',
+    transition: {
+      boxShadow: { ease: 'linear', duration: 10 },
+      backgroundColor: { ease: 'linear', duration: 10 },
+      scale: { type: 'spring', stiffness: 100, damping: 5}
+    }
+  },
+  press: {
+    scale: 1.1,
+    boxShadow: '0px 0px 0px rgba(0,0,0,0.1)',
+    transition: {
+      boxShadow: { ease: 'linear', duration: .1 }
+    }
+  }
 });
 
 const Label = posed.span({

2f2d3fdf7c6e6b46f79ddc0218d2e602ab411aba
Matthew Trabold <mct4240@gmail.com>
2019-05-09 20:02:33 -0500

    Changed File Name

--- client/src/components/authButtons/Login.js
+++ client/src/components/authButtons/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { AuthConsumer } from "../../utils/Auth/authContext";
-import LoginButton from "./practiceButton"
+import LoginButton from "./LoginButton"
 import "./login.css";
 
 const Login = () => (
   <div id="flex">
     <p className="h1 text-center mb-4" id="loginHeader">Chore-It!</p>
-        <LoginButton />
+    <LoginButton />
   </div>
 );
 

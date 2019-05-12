89de8ef801e7e4e7d2c7eb485e16661f5bcc1e9e
Matthew Trabold <mct4240@gmail.com>
2019-05-11 20:44:15 -0500

    Update

--- client/src/components/ProfileComponent/Profile.js
+++ client/src/components/ProfileComponent/Profile.js
@@ -5,13 +5,13 @@ import {AuthConsumer} from "../../utils/Auth/authContext";
 const Profile = () => (
   <AuthConsumer>
     {({user}) => (
-      <div class="flex">
-        <div class="ProfileInformation">
+      <div class="cardContainer">
+        <div class="ProfileInformationContainer">
         <h2 class="profileHeader">Your Profile</h2>
-          <ul class="profile-information">
-            <li>Your Unique ID: {user.id}</li>
-            <li>ID: {user.name}</li>
-            <li>Email: {user.email}</li>
+          <ul class="profileInformationList">
+            <li class="profileListDetail"><h5>Your Unique ID</h5><break/><p>{user.id}</p></li>
+            <li class="profileListDetail"><h5>Username</h5><break/><p>{user.name}</p></li>
+            <li class="profileListDetail"><h5>Email</h5><break/><p>{user.email}</p></li>
           </ul>
         </div>
       </div>
